22bd563135f1e36a6b850a054e4b2588f1dc3dcd
Jean Regisser <jean.regisser@gmail.com>
2024-04-19 11:26:19 +0200

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -146,6 +146,9 @@ export async function gcpHsmToAccount({
       transaction,
       { serializer = serializeTransaction } = {},
     ) {
+      // Copied from https://github.com/wevm/viem/blob/e6c47807f32d14ded53c40831177ee80c5a47a10/src/accounts/utils/signTransaction.ts
+      // TODO: would be nice for this to be done before in viem
+      // so custom Account implementations don't have to worry about it
       const signableTransaction = (() => {
         // For EIP-4844 Transactions, we want to sign the transaction payload body (tx_payload_body) without the sidecars (ie. without the network wrapper).
         // See: https://github.com/ethereum/EIPs/blob/e00f4daa66bd56e2dbd5f1d36d09fd613811a48b/EIPS/eip-4844.md#networking
